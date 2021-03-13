a41b58be943b20020c5bca62af71bd76df44c524
akaluzhskiy <akaluzhskiy@iiko.ru>
2021-03-13 23:04:07 +0400

    config

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.14
 require (
 	github.com/GuiaBolso/darwin v0.0.0-20191218124601-fd6d2aa3d244
 	github.com/go-delve/delve v1.6.0
+	github.com/google/go-cmp v0.5.5
 	github.com/jmoiron/sqlx v1.3.1
 	github.com/lib/pq v1.10.0
+	github.com/pkg/errors v0.9.1
 )
---
+++ internal/platform/database/database.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"net/url"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq" // The database driver in use.
+)
+
+// COnfig for database connection
+type Config struct{
+	Host string
+	Name string
+	User string
+	Password string
+	DisableTLS bool
+}
+
+// Open knows how to open a database connection.
+func Open(conf Config) (*sqlx.DB, error) {
+	// Query parameters.
+	q := make(url.Values)
+	q.Set("sslmode", "require")
+	if conf.DisableTLS {
+		q.Set("sslmode", "disable")
+	}
+	q.Set("timezone", "utc")
+
+	// Construct url.
+	u := url.URL{
+		Scheme:   "postgres",
+		User:     url.UserPassword(conf.User, conf.Password),
+		Host:     conf.Host,
+		Path:     "postgres",
+		RawQuery: q.Encode(),
+	}
+
+	return sqlx.Open("postgres", u.String())
+}
