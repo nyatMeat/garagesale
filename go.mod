47c718c43e06ca07ca18b7ade39baf165a7dee41
akaluzhskiy <akaluzhskiy@iiko.ru>
2021-03-13 19:46:00 +0400

    another lesson

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nyatmeat/garagesale
 
 go 1.14
+
+require github.com/go-delve/delve v1.6.0
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+
+	//App starting
+	log.Println("main: started")
+	defer log.Println("main: completed")
+
+	//Start api service
+	api := http.Server{
+		Addr:         "localhost:20000",
+		Handler:      http.HandlerFunc(ListProducts),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	//make a channel to listen for errors coming from the listener
+	//Use a buffered channel so the gorutine can exit if we don't collect this error
+	serverErrors := make(chan error, 1)
+
+	//start the service listening for requests
+	go func() {
+		log.Printf("main: Api listering on %s", api.Addr)
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	//make a channel to listen for an interupt or terminate signal from OS
+	//Use a buffered channel because the signal package requires it
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	//Shutdown
+
+	//Blocking a main and waiting for shutdown
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("error: Listening and serving: %s", err)
+	case <-shutdown:
+		log.Println("main: Start shutdown")
+
+		const timeout = 5 * time.Second
+
+		// Give outstanding requests a deadline for completion.
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		//Asking listener to shutdown and load shed
+		err := api.Shutdown(ctx)
+		if err != nil {
+			log.Println("main: grateful shutdown did not complete in %v : %v", timeout, err)
+			err = api.Close()
+		}
+		if err != nil {
+			log.Fatalf("main: could not stop server gracefully: %v", err)
+		}
+	}
+}
+
+type Product struct {
+	Name     string `json:"name"`
+	Cost     int    `json:"cost"`
+	Quantity int    `json:"quantity"`
+}
+
+// Echo is a basic HTTP Handler.
+// If you open localhost:20000 in your browser, you may notice
+// double requets being made. This happens because the browser
+// sends a request in the background for a website favicon.
+func ListProducts(w http.ResponseWriter, r *http.Request) {
+	list := []Product{}
+
+	if true {
+		list = append(list, Product{Name: "Comic book", Cost: 85, Quantity: 50})
+		list = append(list, Product{Name: "Mcdonald toys", Cost: 50, Quantity: 30})
+	}
+
+	data, err := json.Marshal(list)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("Error marhalling", err)
+		return
+	}
+	w.Header().Set("content-type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write(data); err != nil {
+		log.Println("Error writing", err)
+	}
+}
